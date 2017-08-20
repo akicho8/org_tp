f55f029d74e95198b2f0d597323cd267e4f930d5
akicho8 <akicho8@gmail.com>
2017-08-20 21:17:41 +0900

    add markdown example

--- examples/0150_markdown_format.rb
+++ examples/0150_markdown_format.rb
@@ -1,7 +1,27 @@
 $LOAD_PATH << '../lib'
 require 'org_tp'
 
-tp [{id: 1, name: 'alice'}, {id: 2, name: 'bob'}], intersection: '|', cover: false
+array = [
+  {id: 1, name: 'alice' },
+  {id: 2, name: 'bob'   },
+]
+
+# tp with options
+tp array, intersection: '|', cover: false
+
+# to_t with options
+array.to_t(intersection: '|', cover: false) # => "| id | name  |\n|----|-------|\n|  1 | alice |\n|  2 | bob   |\n"
+
+puts
+
+# set global options
+OrgTp::Generator.default_options.update(intersection: '|', cover: false)
+tp array
+# >> | id | name  |
+# >> |----|-------|
+# >> |  1 | alice |
+# >> |  2 | bob   |
+# >>
 # >> | id | name  |
 # >> |----|-------|
 # >> |  1 | alice |
